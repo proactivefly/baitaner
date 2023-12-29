2ac3a48a4f7f19b76b538d09fbaf321e5e21e173
罗永浩 <niuguozhen@ouutlook.com>
2023-12-29 23:03:27 +0800

    feat:export default

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,6 @@
 // postcss.config.js
 // postcss 插件是 object 方式注册的话，是按照由上到下的顺序执行的，相关实现见 postcss-load-config
-module.exports = {
+export default  {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default  {
   // 这里给出了一份 taro 通用示例，具体要根据你自己项目的目录结构进行配置
   // 比如你使用 vue3 项目，你就需要把 vue 这个格式也包括进来
   // 不在 content glob表达式中包括的文件，在里面编写tailwindcss class，是不会生成对应的css工具类的
