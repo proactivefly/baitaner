9714b28e576e7e490723f4671a3ac82834c71c99
罗永浩 <niuguozhen@ouutlook.com>
2023-12-30 10:35:42 +0800

    feat:集成了nutui

--- config/index.ts
+++ config/index.ts
@@ -9,16 +9,23 @@ export default defineConfig(async (merge, { command, mode }) => {
   const baseConfig: UserConfigExport = {
     projectName: 'baitaner',
     date: '2023-12-27',
-    designWidth: 750,
+    designWidth (input) {
+      // 配置 NutUI 375 尺寸
+      if (input?.file?.replace(/\\+/g, '/').indexOf('@nutui') > -1) {
+        return 375
+      }
+      // 全局使用 Taro 默认的 750 尺寸
+      return 750
+    },
     deviceRatio: {
       640: 2.34 / 2,
       750: 1,
-      375: 2,
-      828: 1.81 / 2
+      828: 1.81 / 2,
+      375: 2 / 1
     },
     sourceRoot: 'src',
     outputRoot: 'dist',
-    plugins: [],
+    plugins: ['@tarojs/plugin-html'],
     defineConstants: {
     },
     copy: {
--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,5 +1,6 @@
 import { View, Text } from '@tarojs/components'
 import { useLoad } from '@tarojs/taro'
+import { Button } from '@nutui/nutui-react-taro';
 import './index.styl'
 
 export default function Index() {
@@ -11,6 +12,13 @@ export default function Index() {
   return (
     <View className='text-[100px] index p-6'>
       <Text>Hello world! 我的全民摆摊</Text>
+      <Button openType='openSetting'>打开授权设置页</Button>
+      <Button type='primary'>主要按钮</Button>
+      <Button type='info'>信息按钮</Button>
+      <Button type='default'>默认按钮</Button>
+      <Button type='danger'>危险按钮</Button>
+      <Button type='warning'>警告按钮</Button>
+      <Button type='success'>成功按钮</Button>
     </View>
   )
 }
